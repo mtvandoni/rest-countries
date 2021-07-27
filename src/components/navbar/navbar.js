ac92f64f105c5096e4cb9bae8665a7c1757dc485
mica <mtvandoni@gmail.com>
2021-07-26 21:46:17 -0300

    first commit - add project

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,17 @@
+import './navbar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoon } from '@fortawesome/free-solid-svg-icons'
+export const Navbar = ({}) => {
+
+
+	return (
+		<div>
+			<ul>
+				<li><a style={{fontWeight: '800', fontSize: '18px'}}>Where in the world?</a></li>
+        <li style={{float: 'right'}}><a><FontAwesomeIcon icon={faMoon} /> Dark Mode</a></li>
+			</ul>
+		</div>
+	);
+}
+
+export default Navbar;
